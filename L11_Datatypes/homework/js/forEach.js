d811c9640ba1cbe4aaf746379c46e08e1793add3
Andrew Tantsiura <andrii.tans@gmail.com>
2017-07-23 23:37:44 +0300

    changed

--- L11_Datatypes/homework/js/forEach.js
+++ L11_Datatypes/homework/js/forEach.js
@@ -1,7 +1,9 @@
-function forEach(arr, fn){
-	for(var i = 0; i < arr.length; i++){
+function forEach(arr, fn) {
+	for (var i = 0; i < arr.length; i++) {
 		fn(arr[i]);
 	}
 }
 
-forEach([1,2,3], function(el){console.log(el) } );
+forEach([1, 2, 3], function(el) {
+	console.log(el)
+});
