72b231c1eaa2d49f5016a8cd59e20f8af1d91c23
Andrew Tantsiura <andrii.tans@gmail.com>
2017-08-24 00:29:13 +0300

    changed

--- L19_AJAX/index.js
+++ L19_AJAX/index.js
@@ -1,44 +1,23 @@
-// varik  NORM
-var promise = getJSON('http://api.open-notify.org/astrsos.json');
-console.log(typeof promise); // -> “object”
+var promise = getJSON('http://api.open-notify.org/astros.json');
+console.log(typeof promise);
 promise.then(function(data) {
-    console.log(data.message); // -> “success”
-}).catch(function(err){
-    console.log(err);
+	console.log(data.message);
+}, function(err) {
+	console.log(err);
 });
 
 function getJSON(url) {
-  return new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", url);
-    xhr.onload = () => resolve(JSON.parse(xhr.responseText));
-    xhr.onerror = (error) => reject(error);
-    xhr.send();
-  });
-};
+	return new Promise(function(resolve, reject) {
+		fetch(url).then(function(response) {
+			return response.json();
+		}).then(function(returnedValue) {
+			resolve(returnedValue);
+		}).catch(function(err) {
+			reject(err);
+		});
 
-// varik 2
-// var promise = getJSON('http://api.open-notify.org/astros.json');
-// console.log(typeof promise); // -> “object”
-// promise.then(function(data) {
-//     console.log(data.message); // -> “success”
-// }, function(error) {
-//     console.log(error);
-// });
-
-
-// function getJSON(url) {
-//     return new Promise(function(resolve, reject) {
-//         fetch(url).then(function(response) {
-//             return response.json();
-//         }).then(function(returnedValue) {
-//             resolve(returnedValue);
-//         }).catch(function(err) {
-//             reject(err);
-//         });
-
-//     });
-// }
+	});
+}
 
 /////////////////////////////////
 
@@ -59,145 +38,133 @@ let tembObj;
 loadAnim('none', 'block');
 
 function updatedDate(inputDate) {
-    let tempArr = inputDate.split('-');
-    let time, year, month, day, hours, minutes, mid;
-    let monthNames = ["January", "February", "March", "April", "May", "June",
-        "July", "August", "September", "October", "November", "December"
-    ];
-    time = new Date(tempArr[0], tempArr[1], tempArr[2]);
+	let tempArr = inputDate.split('-');
+	let time, year, month, day, hours, minutes, mid;
+	let monthNames = ["January", "February", "March", "April", "May", "June",
+		"July", "August", "September", "October", "November", "December"
+	];
+	
+	time = new Date(tempArr[0], tempArr[1], tempArr[2]);
+	day = time.getDate();
+	day = day < 10 ? '0' + day : day;
+	year = time.getFullYear();
 
-    day = time.getDate();
-    day = day < 10 ? '0' + day : day;
-    year = time.getFullYear();
-
-    return (`UPDATED ON ${monthNames[time.getMonth()-1].toUpperCase()} ${day}, ${year}`);
+	return (`UPDATED ON ${monthNames[time.getMonth()-1].toUpperCase()} ${day}, ${year}`);
 }
 
 function averageTemprature(min, max) {
-    let calc = (max + min) / 2;
-    return Math.floor(calc) + '°С';
+	let calc = (max + min) / 2;
+	return Math.floor(calc) + '°С';
 }
 
-function appendFunc(tMin='null', tMax='null', date, wSpeed='- -', wDirection='- -') {
-    let weatherBlock = document.getElementById('weather_block');
-    let weatherWrapper;
-    let title = [];
-    if (document.getElementById('weatherWrapper') === null) {
-        weatherWrapper = document.createElement('div');
-        weatherWrapper.id = 'weatherWrapper';
-    } else {
-        weatherWrapper = document.getElementById('weatherWrapper');
-    }
-console.log(wSpeed);
-    title[0] = document.createElement('div');
-    title[0].innerHTML = `THE WEATHER ON MARS IS:`;
-    title[0].className = 'titleWeather';
-    title[1] = document.createElement('div');
-    title[1].className = 'tempratureStyle';
-    if (tMin == 'null' || tMax == 'null'){
-    title[1].innerHTML = 'NO INFO';
-    } else {
-    title[1].innerHTML = averageTemprature(tMin, tMax);
-    }
-    title[2] = document.createElement('span');
-    title[2].innerHTML = `Wind speed: ${wSpeed} & `;
-    title[2].className = 'windInfo';
-    title[3] = document.createElement('span');
-    title[3].innerHTML = `Wind direction: ${wDirection}`;
-    title[3].className = 'windInfo';
-    title[4] = document.createElement('div');
-    title[4].innerHTML = updatedDate(date);
+function appendFunc(tMin = 'null', tMax = 'null', date, wSpeed = '- -', wDirection = '- -') {
+	wSpeed = wSpeed == null ? '- -' : '- -';
+	wDirection = wDirection == null ? '- -' : '- -';
+	let weatherBlock = document.getElementById('weather_block');
+	let weatherWrapper;
+	let title = [];
+	if (document.getElementById('weatherWrapper') === null) {
+		weatherWrapper = document.createElement('div');
+		weatherWrapper.id = 'weatherWrapper';
+	} else {
+		weatherWrapper = document.getElementById('weatherWrapper');
+	}
+	title[0] = document.createElement('div');
+	title[0].innerHTML = `THE WEATHER ON MARS IS:`;
+	title[0].className = 'titleWeather';
+	title[1] = document.createElement('div');
+	title[1].className = 'tempratureStyle';
+	if (tMin == 'null' || tMax == 'null') {
+		title[1].innerHTML = 'NO INFO';
+	} else {
+		title[1].innerHTML = averageTemprature(tMin, tMax);
+	}
+	title[2] = document.createElement('span');
+	title[2].innerHTML = `Wind speed: ${wSpeed} & `;
+	title[2].className = 'windInfo';
+	title[3] = document.createElement('span');
+	title[3].innerHTML = `Wind direction: ${wDirection}`;
+	title[3].className = 'windInfo';
+	title[4] = document.createElement('div');
+	title[4].innerHTML = updatedDate(date);
 
-    for (let i = 0; i < title.length; i++) {
-        weatherWrapper.appendChild(title[i]);
-    }
+	for (let i = 0; i < title.length; i++) {
+		weatherWrapper.appendChild(title[i]);
+	}
 
-    weatherBlock.insertBefore(weatherWrapper, document.getElementById('nav_buttons'));
+	weatherBlock.insertBefore(weatherWrapper, document.getElementById('nav_buttons'));
 
 }
 
 function clearFunc() {
-    document.getElementById('weatherWrapper').innerHTML = '';
-    loadAnim('none', 'block');
+	document.getElementById('weatherWrapper').innerHTML = '';
+	loadAnim('none', 'block');
 }
 
 function clearScriptTag(url) {
-    headTag.removeChild(scriptTag);
-    scriptTag = document.createElement('script');
-    scriptTag.setAttribute('src', url);
-    headTag.appendChild(scriptTag);
+	headTag.removeChild(scriptTag);
+	scriptTag = document.createElement('script');
+	scriptTag.setAttribute('src', url);
+	headTag.appendChild(scriptTag);
 }
 
 function getData(data) {
-    loadAnim('block', 'none');
-    console.log(data.report);
 
-    pageNumPrev = data.next;
-    pageNumNext = data.previous;
-    max_in_arr = data.results.length;
-    tembObj = data.results;
-    console.log(tembObj);
-    console.log(data.next);
-    console.log(data.previous);
-
-    appendFunc(tembObj[counter].mins_temp, tembObj[counter].maxs_temp, tembObj[counter].terrestrial_date);
+	loadAnim('block', 'none');
+	pageNumPrev = data.next;
+	pageNumNext = data.previous;
+	max_in_arr = data.results.length;
+	tembObj = data.results;
+	appendFunc(tembObj[counter].min_temp, tembObj[counter].max_temp, tembObj[counter].terrestrial_date, tembObj[counter].wind_speed, tembObj[counter].wind_direction);
 }
 
 document.getElementById('nav_buttons').addEventListener('click', chooseFunc);
 
 function chooseFunc(event) {
-    let directionPrev = true;
-    console.log(counter);
-    let targetEl = event.target || event.srcElement;
-    console.log(targetEl);
+	let directionPrev = true;
+	let targetEl = event.target || event.srcElement;
 
-    if (targetEl.getAttribute('class') === 'prev') {
-        clearFunc();
-        if (counter + 1 == max_in_arr) {
-            console.log('SUCCESS PREVIOUS');
-            clearScriptTag(pageNumPrev);
-            counter = 0;
-            nextPage = false;
-        } else {
-            counter++
-            console.log(counter);
-            console.log(tembObj[counter]);
-            appendFunc(tembObj[counter].min_temp, tembObj[counter].max_temp, tembObj[counter].terrestrial_date);
-            loadAnim('block', 'none');
-        }
-    } else if (targetEl.getAttribute('class') === 'next') {
-        if (counter === 0 && pageNumNext != null) {
-            clearFunc();
-            clearScriptTag(pageNumNext)
-            counter = max_in_arr - 1;
-            nextPage = false;
-        } else if (!counter - 1 < 0) {
-            console.log('SUCCESS');
-            counter--
-            console.log(counter);
-            console.log(tembObj[counter]);
-            clearFunc();
-            appendFunc(tembObj[counter].min_temp, tembObj[counter].max_temp, tembObj[counter].terrestrial_date);
-            loadAnim('block', 'none');
-        } else {
-            console.log('WOOPS');
-        }
-    }
+	if (targetEl.getAttribute('class') === 'prev') {
+		clearFunc();
+		if (counter + 1 == max_in_arr) {
+			clearScriptTag(pageNumPrev);
+			counter = 0;
+			nextPage = false;
+		} else {
+			counter++
+			appendFunc(tembObj[counter].min_temp, tembObj[counter].max_temp, tembObj[counter].terrestrial_date, tembObj[counter].wind_speed, tembObj[counter].wind_direction);
+			loadAnim('block', 'none');
+		}
+	} else if (targetEl.getAttribute('class') === 'next') {
+		if (counter === 0 && pageNumNext != null) {
+			clearFunc();
+			clearScriptTag(pageNumNext)
+			counter = max_in_arr - 1;
+			nextPage = false;
+		} else if (!counter - 1 < 0) {
+			counter--
+			clearFunc();
+			appendFunc(tembObj[counter].min_temp, tembObj[counter].max_temp, tembObj[counter].terrestrial_date);
+			loadAnim('block', 'none');
+		} else {
+			console.log('There is no next page');
+		}
+	}
 }
 
 function loadAnim(block, anim) {
-    if (document.getElementById('anim') === null) {
-        let anim = document.createElement('div');
-        let temp;
-        anim.id = 'anim';
-        for (let i = 1; i <= 3; i++) {
-            temp = document.createElement('div');
-            temp.className = 'rect' + i;
-            anim.appendChild(temp);
-        }
-        document.getElementsByClassName('container')[0].appendChild(anim);
-    }
+	if (document.getElementById('anim') === null) {
+		let anim = document.createElement('div');
+		let temp;
+		anim.id = 'anim';
+		for (let i = 1; i <= 3; i++) {
+			temp = document.createElement('div');
+			temp.className = 'rect' + i;
+			anim.appendChild(temp);
+		}
+		document.getElementsByClassName('container')[0].appendChild(anim);
+	}
 
-    document.getElementById('weather_block').style.display = block;
-    document.getElementById('anim').style.display = anim;
+	document.getElementById('weather_block').style.display = block;
+	document.getElementById('anim').style.display = anim;
 }
